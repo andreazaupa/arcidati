1e9635edb76803f59b70bf99966aacea1161397b
Andrea Zaupa <andrea.zaupa@develon.com>
2011-12-09 12:27:36 +0100

    reinit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Arcidati::Application.config.secret_token = '5c30060bc7ccf62fdef0a14e98875e3e562659f49433a0ff7276a85be5e60075997a290bf7ab72f7b2011e59f3506a3df57de0816b43396f89be331a6ae6205f'
