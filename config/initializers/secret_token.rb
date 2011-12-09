9e58bf799c83d161681b36c77cd46b2b3f8776f9
Andrea Zaupa <andrea.zaupa@develon.com>
2011-12-09 12:26:43 +0100

    remove all

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Arcidati::Application.config.secret_token = '808a7c6669d3bf3660bfc3596bed14979ac668c7d36eddef887afacd9e015664e45b58c361ccb71636d9934c86b67e37050e224d572f21cdc85e8ca4debf6be1'
