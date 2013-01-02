d3dde9bf8a8bd843e644e8529293e1493f3e9bc8
azaupa <azaupa@gmail.com>
2013-01-02 22:23:26 +0100

    revert changes

--- app/admin/cards.rb
+++ app/admin/cards.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register Card do
  form do |f|
       f.inputs "Carta" do
         f.input :code
-        f.input :year, :as=>:string, :input_html => { :value => (f.object.new_record? ? f.object.cur_year : f.object.year ) }  
+        f.input :year#, :as=>:string, :input_html => { :value => (f.object.new_record? ? f.object.cur_year : f.object.year ) }
         f.input :name
         f.input :surname
         f.input :email
@@ -11,9 +11,9 @@ ActiveAdmin.register Card do
         f.input :club
       end
       f.inputs "Dettagli" do
-      	# f.input :birth, :start_year => 1900, :end_year => Time.now.year
-        # f.input :id, :as=>:string, :input_html => { :value => f.object.birth  } 
-        f.input :birth, :as=>:string, :input_html => { :value => I18n.l( f.object.try("date_helper") || Time.now.to_date) } 
+       f.input :birth, :start_year => 1900, :end_year => Time.now.year
+        # f.input :id, :as=>:string, :input_html => { :value => f.object.birth  }
+        # f.input :birth, :as=>:string, :input_html => { :value => I18n.l( f.object.try("date_helper") || Time.now.to_date) }
       	f.input :adress
       	f.input :city
       	f.input :cap
@@ -26,11 +26,11 @@ ActiveAdmin.register Card do
 
 
 
-index do 
+index do
   column :human_name
   column :email
   column :year
   column :code
     default_actions
-end  
+end
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,10 +1,10 @@
 class Card < ActiveRecord::Base
 
-after_initialize :set_year
+# after_initialize :set_year
 
-def set_year
-	self.year = 2013#Time.now.month >= 11 ? Time.now.year+1 : Time.now.year
-end
+# def set_year
+# 	self.year = 2013#Time.now.month >= 11 ? Time.now.year+1 : Time.now.year
+# end
 
 def cur_year
   Time.now.month >= 11 ? Time.now.year+1 : Time.now.year
